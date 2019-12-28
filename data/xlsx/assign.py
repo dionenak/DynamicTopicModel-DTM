645e5c74669e955a8d0e78ca5ca0022623ee7b8f
Nina <dionenak@outlook.com>
2019-12-28 18:32:03 +0100

    delete py script

--- data/xlsx/assign.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import pandas as pd
-from pandas import ExcelWriter
-from pandas import ExcelFile
-
-df = pd.read_excel('march2.xlsx')
-
-df.drop(df.columns[[20,21,22,23,24,25,26]], axis=1, inplace=True)
-print(df.columns)
